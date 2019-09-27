aeb07d448177fe5cf4ce515cc568ff2cddf1fddb
at235am <at235am@gmail.com>
2019-09-27 14:40:58 -0700

    refractored gui elements for easier editing; theres was too much duplicate lines

--- gui.py
+++ gui.py
@@ -1,91 +1,60 @@
 # import main
 import time
+from os import path
 from random import randint
 from tkinter import *
-from tkinter import scrolledtext
+from tkinter import scrolledtext as st
 from tkinter import filedialog
-from os import path
 from QuadgramStatistics import QuadgramStatistics as QS
 import FitnessScorer as FS
+
+# ASSIGNMENT CIPHERTEXTS:
+CIPHERTEXT_1 = "fqjcb rwjwj vnjax bnkhj whxcq nawjv nfxdu mbvnu ujbbf nnc"
+CIPHERTEXT_2 = "oczmz vmzor jocdi bnojv dhvod igdaz admno ojbzo rcvot jprvi oviyv aozmo cvooj ziejt dojig toczr dnzno jahvi fdiyv xcdzq zocznzxjiy"
+CIPHERTEXT_3 = "ejitp spawa qleji taiul rtwll rflrl laoat wsqqj atgac kthls iraoa twlpl qjatw jufrh lhuts qataq itats aittk stqfj cae"
+CIPHERTEXT_4 = "iyhqz ewqin azqej shayz niqbe aheum hnmnj jaqii yuexq ayqkn jbeuq iihed yzhni ifnun sayiz yudhe sqshu qesqa iluym qkque aqaqm oejjs hqzyu jdzqa diesh niznj jayzy uiqhq vayzq shsnj jejjz nshna hnmyt isnae sqfun dqzew qiead zevqi zhnjq shqze udqai jrmtq uishq ifnun siiqa suoij qqfni syyle iszhn bhmei squih nimnx hsead shqmr udquq uaqeu iisqe jshnj oihyy snaxs hqihe lsilu ymhni tyz"
+
+# ASSIGNMENT PLAINTEXT TO BE ENCRYPTED:
+PHRASE_1 = "He who fights with monsters should look to it that he himself does not become a monster. And if you gaze long into an abyss, the abyss also gazes into you."
+
+PHRASE_2 = "There is a theory which states that if ever anybody discovers exactly what the Universe is for and why it is here, it will instantly disappear and be replaced by something even more bizarre and inexplicable. There is another theory which states that this has already happened."
+
+PHRASE_3 = "Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people's hats off - then, I account it high time to get to sea as soon as I can."
 
 DEFAULT_DATA_FILE_PATH = "quadgrams-data.txt"
 class GUI():
     TEXT_FIELD_INPUT_FONT = ("fixedsys 8")
     TEXT_LABEL_FONT = ("TkDefaultFont 8 bold")
-
     ROOT_WINDOW_WIDTH = 600
     ROOT_WINDOW_HEIGHT = 400
     ALPHABET = "abcdefghijklmnopqrstuvwxyz"
-    def __init__(self, length_limit=140):
+
+    def __init__(self, length_limit=120):
         self.qs = QS(DEFAULT_DATA_FILE_PATH)
         self.num_processes = 100
         self.cpu_count = (FS.cpu_count(), FS.cpu_count()/2)[FS.cpu_count() > 8]
         self.length_limit = length_limit
-        # print(self.cpu_count)
-        # self.datafilepathinput = ''
 
-        # def listenerfileexplorer():
-        #     currentDirectory =path.dirname(path.abspath(__file__))
-        #     file = filedialog.askopenfilename(initialdir=currentDirectory)
-            
-        #     self.datafilepathinput.delete(0, END)
-        #     self.datafilepathinput.insert(0, file)
-        #     self.window.focus_set()
-
-        def listenerSettings():
+        def listenerInfo():
             print('x')
-            # settingbgcolor = "gray"
-            # newheight = GUI.ROOT_WINDOW_HEIGHT - 100
-            # newwidth = GUI.ROOT_WINDOW_WIDTH - 100
 
-            # window = Toplevel(self.root)
-            # window.focus_set()
-            # window.configure(background=settingbgcolor)
-            # window.title("Settings")
-            # x_position = int(self.root.winfo_screenwidth()/2 - newwidth/2)
-            # y_position = int(self.root.winfo_screenheight()/2 - newheight/2)
-            # window.geometry(f'{newwidth}x{newheight}+{x_position}+{y_position}')
+        def listenerTexts():
+            settingbgcolor = "gray"
+            newheight = GUI.ROOT_WINDOW_HEIGHT - 100
+            newwidth = GUI.ROOT_WINDOW_WIDTH - 100
 
-            # window.grid_rowconfigure(0, minsize=30)
-            # window.grid_columnconfigure(0, minsize=30)
+            window = Toplevel(self.root)
+            window.focus_set()
+            window.configure(background=settingbgcolor)
+            window.title("Settings")
+            x_position = int(self.root.winfo_screenwidth()/2 - newwidth/2)
+            y_position = int(self.root.winfo_screenheight()/2 - newheight/2)
+            window.geometry(f'{newwidth}x{newheight}+{x_position}+{y_position}')
 
-            # inputframe = Frame(window, bg=settingbgcolor, width=200, height=300)
-            # fileframe = Frame(inputframe, bg=settingbgcolor, width=50)
+            window.grid_rowconfigure(0, minsize=30)
+            window.grid_columnconfigure(0, minsize=30)
 
-            # settingslabelwidth = 13
-            # cpucountlabel = Label(inputframe, text= "CPU Cores: ", font=GUI.TEXT_LABEL_FONT, bg=labelbgcolor, fg=labelfgcolor, anchor="e", width = settingslabelwidth)
-            # numofprocesseslabel = Label(inputframe, text="# of processes: ", font=GUI.TEXT_LABEL_FONT, bg=labelbgcolor, fg=labelfgcolor, anchor="e", width = settingslabelwidth)
-            # lenghtlimitlabel = Label(inputframe, text="Length Limit: ", font=GUI.TEXT_LABEL_FONT, bg=labelbgcolor, fg=labelfgcolor, anchor="e", width = settingslabelwidth)
-            # alphabetlabel = Label(inputframe, text="Alphabet: ", font=GUI.TEXT_LABEL_FONT, bg=labelbgcolor, fg=labelfgcolor, anchor="e", width = settingslabelwidth)
-            # datafilepathlabel = Label(inputframe, text="Data File Path: ", font=GUI.TEXT_LABEL_FONT, bg=labelbgcolor, fg=labelfgcolor, anchor="e", width = settingslabelwidth)
 
-            # cpucountinput = Entry(inputframe,width=4, font=GUI.TEXT_FIELD_INPUT_FONT, relief=FLAT, bg=inputfieldcolor1)
-            # numofprocessesinput = Entry(inputframe,width=4, font=GUI.TEXT_FIELD_INPUT_FONT, relief=FLAT, bg=inputfieldcolor1)
-            # lengthlimitinput = Entry(inputframe,width=4, font=GUI.TEXT_FIELD_INPUT_FONT, relief=FLAT, bg=inputfieldcolor1)
-            # alphabetinput = Entry(inputframe,width=30, font=GUI.TEXT_FIELD_INPUT_FONT, relief=FLAT, bg=inputfieldcolor1)
-            # self.datafilepathinput = Entry(fileframe,width=23, font=GUI.TEXT_FIELD_INPUT_FONT, relief=FLAT, bg=inputfieldcolor1)
-            # browseforfilebtn = Button(fileframe, text="Browse", command=listenerfileexplorer, width=buttonwidth, height=buttonheight, bg=buttoncolor, relief=FLAT, font=GUI.TEXT_LABEL_FONT, fg=labelfgcolor, overrelief=FLAT, borderwidth=0)
-            
-
-            # inputframe.grid(column=1, row=1, pady=(5,5))
-            # fileframe.grid(column=1, row=4, pady=(0,5), sticky="we")
-            # cpucountlabel.grid(column=0,row=0, pady=(0,5), padx=(0,2))
-            # cpucountinput.grid(column=1,row=0, pady=(0,5), sticky="w")
-            # numofprocesseslabel.grid(column=0,row=1, pady=(0,5), padx=(0,2))
-            # numofprocessesinput.grid(column=1,row=1, pady=(0,5), sticky="w")
-            # lenghtlimitlabel.grid(column=0,row=2, pady=(0,5), padx=(0,2))
-            # lengthlimitinput.grid(column=1,row=2, pady=(0,5), sticky="w")
-            # alphabetlabel.grid(column=0,row=3, pady=(0,5), padx=(0,2))
-            # alphabetinput.grid(column=1,row=3, pady=(0,5))
-            # datafilepathlabel.grid(column=0,row=4, pady=(0,5), padx=(0,2))
-            # self.datafilepathinput.grid(column=1,row=0, pady=(0,0), sticky="w")
-            # browseforfilebtn.grid(column=2,row=0, pady=(0,0), padx=(2,0), sticky="e")
-
-            # # test = Label(window, text="KEY (leave blank to find/generate one):", font=GUI.TEXT_LABEL_FONT, bg=labelbgcolor, fg=labelfgcolor, width=labelbgwidth)
-            # # test.grid(column=0, row=0)
-            # self.window = window
-
-        def listenerHelp():
             textinput.delete("1.0", END)
             res = "HAVENT DONE YET"
             textinput.insert(INSERT, res)
@@ -166,82 +135,87 @@ class GUI():
             resultoutput.config(state="disabled")
             keyusedoutput.config(state="readonly")
         
-        bgcolor = "#515358"
-        labelbgcolor = "#09BA7C"
-        labelfgcolor = "white"
-        labelbgwidth = 34
-        buttoncolor = labelbgcolor
-        buttonwidth = 7
-        buttonheight = 1
-        btngapwidth = 1
+        # color and dimension values:
+        bgcolor          = "#515358"
+        maincolor        = "#09BA7C"
+        maintextcolor    = "white"
         inputfieldcolor1 = "white"
         inputfieldcolor2 = "#C6C6C6"
+        buttonwidth      = 7
+        buttonheight     = 1
+        btngapwidth      = 4
 
+        # style arguments for various widgets:
+        btnStyleArgs     = {'width':buttonwidth, 'height':buttonheight, 'bg':maincolor, 'relief':FLAT, 'font':GUI.TEXT_LABEL_FONT, 'fg':maintextcolor, 'borderwidth':0}
+        labelStyleArgs   = {'font':GUI.TEXT_LABEL_FONT, 'bg':maincolor, 'fg':maintextcolor}
+        inputFieldActiveStyleArgs = {'font':GUI.TEXT_FIELD_INPUT_FONT, 'bg':inputfieldcolor1, 'relief':FLAT}
+        inputFriendDisabledStyleArgs = {'font':GUI.TEXT_FIELD_INPUT_FONT, 'bg':inputfieldcolor2, 'relief':FLAT}
+
+        # creates and sets the root window:
         root = Tk()
         root.configure(background=bgcolor)
         root.title("Simple Substitution Encryption and Decryption")
+
+        # determines the dimensions and initial position of the window (center):
         x_position = int(root.winfo_screenwidth()/2 - GUI.ROOT_WINDOW_WIDTH/2)
         y_position = int(root.winfo_screenheight()/2 - GUI.ROOT_WINDOW_HEIGHT/2)
         root.geometry(f'{GUI.ROOT_WINDOW_WIDTH}x{GUI.ROOT_WINDOW_HEIGHT}+{x_position}+{y_position}')
         
-        # create the two bottom frames:
-        encrypt_decrypt_btn_frame = Frame(root, bg=bgcolor)
-        time_stat_frame = Frame(root, bg=labelbgcolor)
-        textlabelframe = Frame(root, bg=labelbgcolor)
-        textlabelframe.grid(column=1, row=4, sticky='we')
-        resultlabelframe = Frame(root, bg=labelbgcolor)
-        resultlabelframe.grid(column=3, row=4, sticky='we')
+        # creates the two bottom frames:
+        button_frame     = Frame(root, bg=bgcolor)
+        time_stat_frame  = Frame(root, bg=maincolor)
 
-        # create the labels and text boxes:
-        keyinputlabel = Label(root, text="KEY (leave blank to find/generate one):", font=GUI.TEXT_LABEL_FONT, bg=labelbgcolor, fg=labelfgcolor, width=labelbgwidth)
-        keyinput = Entry(root,width=30, font=GUI.TEXT_FIELD_INPUT_FONT, relief=FLAT, bg=inputfieldcolor1)
-        keyusedlabel = Label(root, text="KEY USED:", font=GUI.TEXT_LABEL_FONT, bg=labelbgcolor, fg=labelfgcolor, width=labelbgwidth)     
-        keyusedoutput = Entry(root,width=30, font=GUI.TEXT_FIELD_INPUT_FONT, bg=inputfieldcolor2, relief=FLAT, state="readonly", readonlybackground=inputfieldcolor2) 
-        textinputlabel = Label(textlabelframe, text="TEXT:", font=GUI.TEXT_LABEL_FONT, bg=labelbgcolor, fg=labelfgcolor, width=labelbgwidth)
-        textinput = scrolledtext.ScrolledText(root, width=28, height=15, font=GUI.TEXT_FIELD_INPUT_FONT, relief=FLAT, bg=inputfieldcolor1)
-        resultoutputlabel = Label(resultlabelframe, text="RESULT:", font=GUI.TEXT_LABEL_FONT, bg=labelbgcolor, fg=labelfgcolor, width=labelbgwidth)
-        resultoutput = scrolledtext.ScrolledText(root, width=28, height=15, font=GUI.TEXT_FIELD_INPUT_FONT, bg=inputfieldcolor2, relief=FLAT, state="disabled")
+        # creates the labels and text box widgets:
+        keyinputlabel     =           Label(root, **labelStyleArgs,               text="KEY (leave blank to find/generate one):")
+        keyusedlabel      =           Label(root, **labelStyleArgs,               text="KEY USED:")   
+        textinputlabel    =           Label(root, **labelStyleArgs,               text="TEXT:")
+        resultoutputlabel =           Label(root, **labelStyleArgs,               text="RESULT:")
+        keyinput          =           Entry(root, **inputFieldActiveStyleArgs,    width=30)
+        keyusedoutput     =           Entry(root, **inputFriendDisabledStyleArgs, width=30, state="readonly", readonlybackground=inputfieldcolor2) 
+        textinput         = st.ScrolledText(root, **inputFieldActiveStyleArgs,    width=28, height=15)
+        resultoutput      = st.ScrolledText(root, **inputFriendDisabledStyleArgs, width=28, height=15, state="disabled")
 
-        # create the buttons:
-        settingsbtn = Button(encrypt_decrypt_btn_frame, text="Settings", command=listenerSettings, width=buttonwidth, height=buttonheight, bg=buttoncolor, relief=FLAT, font=GUI.TEXT_LABEL_FONT, fg=labelfgcolor, overrelief=FLAT, borderwidth=0)
-        helpbtn = Button(encrypt_decrypt_btn_frame, text="Help", command=listenerHelp, width=buttonwidth , height=buttonheight,bg=buttoncolor, relief=FLAT, font=GUI.TEXT_LABEL_FONT, fg=labelfgcolor, borderwidth=0)
-        encryptbtn = Button(encrypt_decrypt_btn_frame, text="Encrypt", command=listenerEncrypt, width=buttonwidth+1 , height=buttonheight,bg=buttoncolor, relief=FLAT, font=GUI.TEXT_LABEL_FONT, fg=labelfgcolor, borderwidth=0)
-        decryptbtn = Button(encrypt_decrypt_btn_frame, text="Decrypt", command=listenerDecrypt, width=buttonwidth+1 , height=buttonheight,bg=buttoncolor, relief=FLAT, font=GUI.TEXT_LABEL_FONT, fg=labelfgcolor, borderwidth=0)
+        # creates the buttons:
+        textsbtn    = Button(button_frame, text="Info",    command=listenerInfo,     **btnStyleArgs)
+        examplesbtn = Button(button_frame, text="Texts",   command=listenerTexts, **btnStyleArgs)
+        encryptbtn  = Button(button_frame, text="Encrypt", command=listenerEncrypt,  **btnStyleArgs)
+        decryptbtn  = Button(button_frame, text="Decrypt", command=listenerDecrypt,  **btnStyleArgs)
 
-        # create the time elapsed widgets:
-        timeelapsedlabel = Label(time_stat_frame, text="Time Elapsed:", font=GUI.TEXT_LABEL_FONT, bg=labelbgcolor, fg=labelfgcolor)
-        timevalueplaceholder = Label(time_stat_frame, text="0.0000000000", font=GUI.TEXT_LABEL_FONT, bg=labelbgcolor, fg=labelfgcolor)
-        timeunitlabel = Label(time_stat_frame, text="seconds", font=GUI.TEXT_LABEL_FONT, bg=labelbgcolor, fg=labelfgcolor)
+        # creates the time elapsed widgets:
+        timeelapsedlabel     = Label(time_stat_frame, text="Time Elapsed:", **labelStyleArgs)
+        timevalueplaceholder = Label(time_stat_frame, text="0.0000000000",  **labelStyleArgs)
+        timeunitlabel        = Label(time_stat_frame, text="seconds",       **labelStyleArgs)
         
+        # creates spacing in the gui layout without setting paddings:
         root.grid_columnconfigure(0, minsize=30)
-        root.grid_columnconfigure(2, minsize=50)
         root.grid_rowconfigure(0, minsize=30)
+        root.grid_columnconfigure(2, minsize=50)
         root.grid_rowconfigure(3, minsize=15)
 
-        keyinputlabel.grid(column=1, row=1, sticky="W")
-        keyinput.grid(column=1, row=2, sticky="W")
-        keyusedlabel.grid(column=3, row=1, sticky="W")
-        keyusedoutput.grid(column=3, row=2, sticky="W")
-        textinputlabel.grid(column=0, row=0, sticky="W")
-        textinput.grid(column=1, row=5, sticky="W")
-        resultoutputlabel.grid(column=0, row=0, sticky="W")
-        resultoutput.grid(column=3, row=5, sticky="W")
-        encrypt_decrypt_btn_frame.grid(column=1, row=6, sticky='we')
+        # layouts the labels and text box widgets:
+        keyinputlabel.       grid(column=1, row=1, sticky="we")
+        keyinput.            grid(column=1, row=2, sticky="we")
+        keyusedlabel.        grid(column=3, row=1, sticky="we")
+        keyusedoutput.       grid(column=3, row=2, sticky="we")
+        textinputlabel.      grid(column=1, row=4, sticky="we")
+        textinput.           grid(column=1, row=5, sticky="we")
+        resultoutputlabel.   grid(column=3, row=4, sticky="we")
+        resultoutput.        grid(column=3, row=5, sticky="we")
 
-        settingsbtn.grid(column=0, row=0, pady=5, padx=(2,btngapwidth))
-        helpbtn.grid(column=1, row=0, pady=5, padx=btngapwidth)
-        encryptbtn.grid(column=2, row=0, pady=5, padx=btngapwidth)
-        decryptbtn.grid(column=3, row=0, pady=5, padx=(btngapwidth, 0))
+        # layouts the button frame and the buttons that go into the frame:
+        button_frame.        grid(column=1, row=6, sticky='we')
+        textsbtn.            grid(column=0, row=0, pady=5, padx=(1, btngapwidth))
+        examplesbtn.         grid(column=1, row=0, pady=5, padx=btngapwidth)
+        encryptbtn.          grid(column=2, row=0, pady=5, padx=btngapwidth)
+        decryptbtn.          grid(column=3, row=0, pady=5, padx=(btngapwidth, 0))
 
-        
-        time_stat_frame.grid(column=3, row=6, sticky='we')
-        time_stat_frame.grid_columnconfigure(0, weight=1)
-        time_stat_frame.grid_columnconfigure(1, weight=1)
+        # layouts the time frame and the time-related widgets that belong in the frame:
+        time_stat_frame.     grid(column=3, row=6, sticky='we')
+        timeelapsedlabel.    grid(column=0, row=0, pady=5, padx=(5,0))
+        timevalueplaceholder.grid(column=1, row=0, pady=5, padx=(22,0))
+        timeunitlabel.       grid(column=2, row=0, pady=5, padx=(0,5))
 
-        timeelapsedlabel.grid(column=0, row=0, padx=(5,0), pady=5, sticky="W")
-        timevalueplaceholder.grid(column=1, row=0, pady=5, sticky="E")
-        timeunitlabel.grid(column=2, row=0, padx= (0,20), pady=5, sticky="E")
-
+        # assigns all the the work above to a variable:
         self.root = root
 
 if __name__ == "__main__":
